ddef843c94f5bef25863af84f0514e9770c35e27
XanderB88 <alexxanderbykov@gmail.com>
2021-08-23 04:41:28 +0400

    Add a navigation button and a navigation to the ConfigurationScreen

---
+++ Shared/NavigationButton.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationButton.swift
+//  PomodoroTimer (iOS)
+//
+//  Created by Alexander on 23.08.2021.
+//
+
+import SwiftUI
+
+struct NavigationButton: View {
+
+    var body: some View {
+            NavigationLink(
+                destination: ConfigurationScreen(viewModel: ConfigurationViewModel()),
+                label: {
+                    Image(systemName: "gear")
+                        .gradientForeground(colors: [Color(#colorLiteral(red: 0.8235294118, green: 0.8392156863, blue: 0.937254902, alpha: 1)), Color(#colorLiteral(red: 0.5725490196, green: 0.6, blue: 0.7607843137, alpha: 1))])
+                        .font(.custom("Quicksand-Regular", size: 25))
+                })
+                .buttonStyle(ButtonCofigurationStyle())
+    }
+}
+
+
+struct NavigationButton_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationButton()
+    }
+}
--- Shared/Views/MainScreenView.swift
+++ Shared/Views/MainScreenView.swift
@@ -11,42 +11,43 @@ struct MainScreenView: View {
     @ObservedObject var viewModel: MainViewModel
     
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2470588235, green: 0.2705882353, blue: 0.3960784314, alpha: 1)), Color(#colorLiteral(red: 0.168627451, green: 0.1882352941, blue: 0.2941176471, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack(alignment: .center, spacing: 30) {
-                HStack {
-                    ButtonConfiguraton(buttonSymbol: viewModel.buttonBellState ? "bell.slash" : "bell", function: viewModel.muteTimer, changes: viewModel.buttonBellStateChanged)
+        NavigationView {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2470588235, green: 0.2705882353, blue: 0.3960784314, alpha: 1)), Color(#colorLiteral(red: 0.168627451, green: 0.1882352941, blue: 0.2941176471, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(alignment: .center, spacing: 30) {
+                    HStack {
+                        ButtonConfiguraton(buttonSymbol: viewModel.buttonBellState ? "bell.slash" : "bell", function: viewModel.muteTimer, changes: viewModel.buttonBellStateChanged)
+                        Spacer()
+                        Text("Pomodoro")
+                            .gradientForeground(colors: [Color(#colorLiteral(red: 0.8235294118, green: 0.8392156863, blue: 0.937254902, alpha: 1)), Color(#colorLiteral(red: 0.5725490196, green: 0.6, blue: 0.7607843137, alpha: 1))])
+                            .font(.custom("Quicksand-Regular", size: 32))
+                        Spacer()
+                        NavigationButton()
+                    }
+                    .padding()
                     Spacer()
-                    Text("Pomodoro")
+                    CountDownTimer()
+                    Spacer()
+                    Text("Work")
                         .gradientForeground(colors: [Color(#colorLiteral(red: 0.8235294118, green: 0.8392156863, blue: 0.937254902, alpha: 1)), Color(#colorLiteral(red: 0.5725490196, green: 0.6, blue: 0.7607843137, alpha: 1))])
-                        .font(.custom("Quicksand-Regular", size: 32))
+                        .font(.custom("Quicksand-Regular", size: 26))
                     Spacer()
-                    ButtonConfiguraton(buttonSymbol: "gear", function: viewModel.nothing, changes: viewModel.buttonConfigStateChanged)
-                }
-                .padding()
-                Spacer()
-                CountDownTimer()
-                Spacer()
-                Text("Work")
-                    .gradientForeground(colors: [Color(#colorLiteral(red: 0.8235294118, green: 0.8392156863, blue: 0.937254902, alpha: 1)), Color(#colorLiteral(red: 0.5725490196, green: 0.6, blue: 0.7607843137, alpha: 1))])
-                    .font(.custom("Quicksand-Regular", size: 26))
-                Spacer()
-                HStack {
-                    Spacer()
-                    ButtonAction(buttonSymbol: viewModel.buttonPlayState ? "pause" : "play", function: viewModel.startTimer, changes: viewModel.buttonPlayStateChanged)
-                    Spacer()
-                    ButtonAction(buttonSymbol: "stop", function: viewModel.stopTimer, changes: viewModel.buttonStopStateChanged)
+                    HStack {
+                        Spacer()
+                        ButtonAction(buttonSymbol: viewModel.buttonPlayState ? "pause" : "play", function: viewModel.startTimer, changes: viewModel.buttonPlayStateChanged)
+                        Spacer()
+                        ButtonAction(buttonSymbol: "stop", function: viewModel.stopTimer, changes: viewModel.buttonStopStateChanged)
+                        Spacer()
+                    }
                     Spacer()
                 }
-                Spacer()
+                
             }
-            
         }
     }
 }
-
 struct MainScreenView_Previews: PreviewProvider {
     static var previews: some View {
         MainScreenView(viewModel: MainViewModel())
